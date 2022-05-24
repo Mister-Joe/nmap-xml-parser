7de1af89fa35257fe6f745d1b03518203483e449
Mister-Joe <tredeau@gmail.com>
2022-05-24 14:52:03 -0500

    Bug fix
    Corrected indentation to 4 spaces
    Fixed newline bug in ports_per_host()
    Removed printing of duplicate port numbers

--- nmap-xml-parser.py
+++ nmap-xml-parser.py
@@ -3,44 +3,48 @@ import xml.etree.ElementTree as ET
 
 
 def open_ports():
-        for host in root.iter('ports'):
-                ports = host.findall('port')
-                for port in ports:
-                        print(port.get('portid'))
+    for host in root.iter('ports'):
+        ports = host.findall('port')
+        used_ports = []
+        for port in ports:
+            if port.get('portid') not in used_ports:
+                used_ports.append(port.get('portid'))
+    for port in used_ports:
+        print(port)
 
 def ports_per_host():
-        for host in root.iter('host'):
-                address = host.find('address')
-                print(address.get('addr'), end=' : ')
-                for ports in host.iter('ports'):
-                        port_list = ports.findall('port')
-                        for port in port_list:
-                                print(port.get('portid'), end=',')
+    for host in root.iter('host'):
+        address = host.find('address')
+        print(address.get('addr'), end=' : ')
+        for ports in host.iter('ports'):
+            port_list = ports.findall('port')
+            for port in port_list:
+                print(port.get('portid'), end=',')
         print()
 
 def alive_hosts():
-        for host in root.iter('host'):
-                status = host.find('status')
-                address = host.find('address')
-                if status.get('state') == 'up':
-                        print(address.get('addr'))
+    for host in root.iter('host'):
+        status = host.find('status')
+        address = host.find('address')
+        if status.get('state') == 'up':
+            print(address.get('addr'))
 
 parser = argparse.ArgumentParser(description='Parse nmap XML output.')
 parser.add_argument('filepath', type = str, help = 'path to XML file')
-parser.add_argument('--all_open_ports', action = 'store_true', help = 'display list of open ports irrespective of host')
+parser.add_argument('--all_ports', action = 'store_true', help = 'display list of open ports irrespective of host')
 parser.add_argument('--alive_hosts', action = 'store_true', help = 'display alive hosts (ping + ports)')
-parser.add_argument('--host_port', action = 'store_true', help = 'display hosts & respective open ports')
+parser.add_argument('--host_ports', action = 'store_true', help = 'display hosts & respective open ports')
 args = parser.parse_args()
 
 tree = ET.parse(args.filepath)
 root = tree.getroot()
 
 if len(sys.argv) > 3:
-        print('Error: too many arguments.')
-        exit(-1)
-elif args.all_open_ports == True:
-        open_ports()
+    print('Error: too many arguments.')
+    exit(-1)
+elif args.all_ports == True:
+    open_ports()
 elif args.alive_hosts == True:
-        alive_hosts()
-elif args.host_port == True:
-        ports_per_host()
+    alive_hosts()
+elif args.host_ports == True:
+    ports_per_host()
